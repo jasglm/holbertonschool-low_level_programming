a9df924d085f043dae911e53a41c3489917349a2
jasglm <95955639+jasglm@users.noreply.github.com>
2022-02-04 22:43:15 -0600

    Adding 6-print_numberz.c fix

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry point for function
+ *
+ * Description: Print numbres 0-9 using putchar
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 58; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}

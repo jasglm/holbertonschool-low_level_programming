26d502f684974c24753fec608a8e12d72eb7a376
jasglm <95955639+jasglm@users.noreply.github.com>
2022-02-04 22:17:55 -0600

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - entry point for function
+ *
+ * Description: Print all single numbres base 10 starting from 0
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++) 
+		{
+			printf("%i\n", n);
+		}
+	return (0);
+}

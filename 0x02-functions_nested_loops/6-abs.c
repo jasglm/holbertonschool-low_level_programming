a57db89340803c915a23a6094bbd28c5e28dbd9f
Jaasiel López <jglm88_lp@hotmail.com>
2022-02-08 22:24:55 -0600

    adding 6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _abs - prints absolute value of int
+ * @n: character being exmined
+ * Return: value of int
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (-n);
+	}
+}

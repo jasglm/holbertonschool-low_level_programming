16fe61fdc2800242f767e5eb843d6743f2638400
Jaasiel López <jglm88_lp@hotmail.com>
2022-02-10 21:40:25 -0600

    adding 9 fizz buzz fixed

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - Prints the numbers from 1-100, multiples of three,
- *        Fizz is printed instead of the number, for multiples of five,
- *        Buzz, and for multiples of both three and five, FizzBuzz.
+ * main - Prints the numbers from 1-100, if multiple of three,
+ *        Fizz is printed instead of the number, if multiple of five,
+ *        Buzz, and if multiple of both three and five, FizzBuzz.
  *
  * Return: Always 0.
  */
@@ -13,19 +13,19 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0 && i % 5 == 0)
+		if ((i % 3) == 0 && (i % 5) == 0)
 			printf("FizzBuzz");
 
-		else if (i % 3 == 0)
+		else if ((i % 3) == 0)
 			printf("Fizz");
 
-		else if (i % 5 == 0)
+		else if ((i % 5) == 0)
 			printf("Buzz");
 
 		else
 			printf("%d", i);
 
-		if (num < 100)
+		if (i < 100)
 			printf(" ");
 	}
 

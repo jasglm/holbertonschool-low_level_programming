60500ee26a3966264aecea7188f21fd8b4b60b3c
Jaasiel López <jglm88_lp@hotmail.com>
2022-03-10 21:58:35 -0600

    fix 5 advance task

--- 0x10-variadic_functions/100-hello_world.asm
+++ 0x10-variadic_functions/100-hello_world.asm
@@ -1,14 +1,18 @@
-	global	main
+global _start
 
-	section	.text
-main:	mov	rax, 1
-	mov	rdi, 1
-	mov	rsi, message
-	mov	rdx, 17
-	syscall
-	mov	rax, 60
-	xor	rdi, rdi
-	syscall
+section .text
 
-	section	.data
-message:db	"Hello, World", 10
+_start:
+  mov rax, 1
+  mov rdi, 1
+  mov rsi, msg
+  mov rdx, msglen
+  syscall
+
+  mov rax, 60
+  mov rdi, 0
+  syscall
+
+section .rodata
+  msg: db "Hello, world", 10
+  msglen: equ $ - msg
